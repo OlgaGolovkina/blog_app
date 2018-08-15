6c7ab7ef27fbdc6c092b2fa1fa0a253f60a3124c
OlgaGolovkina <ogolovkina13@gmail.com>
2018-08-15 19:16:31 +0300

    List of articles page

--- blog.py
+++ blog.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
+from data import articles
 
 app = Flask(__name__)
 app.debug = True
+
+objects = articles()
 
 
 @app.route('/')
@@ -17,3 +20,13 @@ def index():
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+
+@app.route('/articles')
+def articles():
+    return render_template("articles.html", articles=objects)
+
+
+# @app.route('/articles/<string:id>')
+# def articles(id):
+#     return render_template("articles.html", id=id)
---
+++ data.py
@@ -0,0 +1,30 @@
+def articles():
+
+    objects = [
+        {
+            "id": 1,
+            "title": "Article One",
+            "body": "'John Doe' and 'Jane Doe' are multiple-use names that are"
+                    " used in the case of somebody wishing to have the party"
+                    " who is given the name to remain anonymous.",
+            "author": "John Doe",
+            "create_date": "15-08-2018",
+        },
+        {
+            "id": 2,
+            "title": "Article Two",
+            "body": "'John Doe' and 'Jane Doe' are multiple-use names that are"
+                    " used in the case of somebody wishing to have the party"
+                    " who is given the name to remain anonymous.",
+            "author": "Jane Doe",
+            "create_date": "15-08-2018",
+        },
+        {
+            "id": 3,
+            "title": "Article Three",
+            "body": "Really cool dude!",
+            "author": "Bob Dylan",
+            "create_date": "15-08-2018",
+        },
+    ]
+    return objects
